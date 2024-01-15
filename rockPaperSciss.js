11e27fddda8009a1072da38157ab29e419cb8c35
Manish_Dubey <131174682+Manishdubey9331@users.noreply.github.com>
2024-01-15 22:14:17 +0530

    Update rockPaperSciss.js

--- rockPaperSciss.js
+++ rockPaperSciss.js
@@ -6,10 +6,13 @@ let ccount=document.getElementById("compCount")
 let clr=document.getElementById("btn")
   let  uc=0;
    let cc=0;
-   clr.addEventListener("click",function(){
-    ucount.innerHTML=0;
-    ccount.innerHTML=0;
-   })
+   function clrs(){
+    uc=0;
+    cc=0;
+    ucount.innerHTML=   `${uc}`;
+    ccount.innerHTML=`${cc}`;
+   }
+   clr.addEventListener("click",clrs)
 function comp(){
  const arr=['scissor','paper','rock']
 let com=Math.floor(Math.random()*3)
@@ -27,12 +30,18 @@ function showwinner(userwin,uch,cch){
         start.style.backgroundColor=" #dc1515f5"
     }
 }
+function draw(cchoice,uchoice){
+    clrs()
+    start.innerHTML=`game draw as ${uchoice} and ${cchoice} are same`
+    start.style.backgroundColor="#cdde30";
+}
 function playgame(uchoice){
     let userwin=true;
     let cchoice=comp();
     if(cchoice===uchoice){
     console.log("draw game")
-    start.innerText("game draw")}
+   draw(cchoice,uchoice)
+        }
     else{
         if(uchoice ==="scissor" && cchoice==="paper" ||
          uchoice==="paper" && cchoice==="rock" || 
